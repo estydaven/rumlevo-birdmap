2090993bb717125c0b72e2a81a70083dcf6c73ee
estydaven <esetnod1111@gmail.com>
2025-07-24 20:18:32 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
-// import fs from 'fs'
-// import path from 'path'
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-//   base: 'rumlevo-birdmap',
-//     server: {
-//     https: {
-//       key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-//       cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-//     }
-//   }
+  base: 'rumlevo-birdmap'
 });
 
