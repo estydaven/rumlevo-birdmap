c0a0cc6999eae45f7a95d2af544ec728889ef7bc
estydaven <esetnod1111@gmail.com>
2025-07-25 10:48:11 +0300

    fix: add 404 redirect and nojekyll

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   base: '/rumlevo-birdmap/',
-});
-
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    }
+  }
+})
